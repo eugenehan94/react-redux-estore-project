4ab59e23c615a51c8be705bf3c2859e3c1b050be
eugenehan94 <eugene.han94@gmail.com>
2021-10-01 23:13:16 -0400

    comment out dev tools, so github pages would work

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,8 +6,8 @@ const store = createStore(
   reducers,
   {},
   compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
